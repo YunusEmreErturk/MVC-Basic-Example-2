a2133da55bbdb0f88679ab1a770278f771547f13
Yunus Emre Ertürk <45642601+YunusEmreErturk@users.noreply.github.com>
2019-04-03 21:12:38 +0300

    allproject

---
+++ Sirket/Controllers/SirketHomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sirket.Controllers
+{
+    public class SirketHomeController : Controller
+    {
+        // GET: SirketHome
+        SirketEntities db = new SirketEntities();
+        
+        public ActionResult Index()
+        {
+            List<Personel> personelList = db.Personels.ToList();
+            return View(personelList);
+        }
+    }
+}
